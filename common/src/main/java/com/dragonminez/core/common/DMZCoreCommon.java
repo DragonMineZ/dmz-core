a2c4064762bd91ba3eea07247ce03950abc24e9f
Sleep <83037839+KyoSleep1@users.noreply.github.com>
2025-11-07 16:22:27 -0700

    feat: finish project structure

--- common/src/main/java/com/dragonminez/core/common/DMZCoreCommon.java
+++ common/src/main/java/com/dragonminez/core/common/DMZCoreCommon.java
@@ -1,7 +1,9 @@
 package com.dragonminez.core.common;
 
 import com.dragonminez.core.common.util.LogUtil;
+import net.minecraftforge.fml.common.Mod;
 
+@Mod(Reference.MOD_ID + "_common")
 public final class DMZCoreCommon {
 
     public static void init(Env env) {
