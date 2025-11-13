5bdf8da548da8e8823cb38abca00a874afaa319f
Sleep <83037839+KyoSleep1@users.noreply.github.com>
2025-11-12 19:44:14 -0700

    feat: add isDebug function on Env class

--- common/src/main/java/com/dragonminez/core/common/Env.java
+++ common/src/main/java/com/dragonminez/core/common/Env.java
@@ -1,6 +1,12 @@
 package com.dragonminez.core.common;
 
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
 public enum Env {
-    CLIENT,
-    SERVER
+  CLIENT,
+  SERVER;
+
+  public static boolean isDebug() {
+    return !FMLEnvironment.production;
+  }
 }
